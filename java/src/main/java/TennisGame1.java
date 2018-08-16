47d02deb853f220d5fc7211c4c085fa963384838
Oliver Michels <oliver.michels@getit.de>
2018-08-16 10:06:57 +0200

    extracted player, extracted some methods, some cleanup

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,76 +1,87 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private TennisPlayer player1;
+    private TennisPlayer player2;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new TennisPlayer(player1Name);
+        this.player2 = new TennisPlayer(player2Name);
     }
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
-    }
+	public void wonPoint(String playerName) {
+		if (playerName.equals(player1.getName()))
+			player1.incrementScore();
+		else
+			player2.incrementScore();
+	}
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1.getScore() == player2.getScore())
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = evaluateWhenBalanced(player1.getScore());
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1.getScore() >=4 || player2.getScore() >=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = evaluateAdvantage(player1.getScore(), player2.getScore());
         }
         else
         {
+        	int tempScore=0;
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                if (i==1) tempScore = player1.getScore();
+                else { score+="-"; tempScore = player2.getScore();}
+                score = translate2TennisSlang(score, tempScore);
             }
         }
         return score;
     }
+
+	protected String translate2TennisSlang(String score, int tempScore) {
+		switch(tempScore)
+		{
+		    case 0:
+		        score+="Love";
+		        break;
+		    case 1:
+		        score+="Fifteen";
+		        break;
+		    case 2:
+		        score+="Thirty";
+		        break;
+		    case 3:
+		        score+="Forty";
+		        break;
+		}
+		return score;
+	}
+
+	protected String evaluateWhenBalanced(int score) {
+		switch (score) {
+		case 0:
+			return "Love-All";
+		case 1:
+			return "Fifteen-All";
+		case 2:
+			return "Thirty-All";
+		default:
+			return "Deuce";
+		}
+	}
+
+	protected String evaluateAdvantage(int scorePlayer1, int scorePlayer2) {
+		String score;
+		int minusResult = scorePlayer1 - scorePlayer2;
+		if (minusResult == 1)
+			score = "Advantage " + player1.getName();
+		else if (minusResult == -1)
+			score = "Advantage " + player2.getName();
+		else if (minusResult >= 2)
+			score = "Win for " + player1.getName();
+		else
+			score = "Win for " + player2.getName();
+		return score;
+	}
 }
